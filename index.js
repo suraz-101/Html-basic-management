f1e91d5512a4a05d77f53841a2bbdeeea25d3235
Suraj Pandey <surazpandey101@gmail.com>
2024-02-26 09:52:42 +0545

    learning to wrape form data into an object

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const showMyAge = () => {
-  const Age = document.getElementById("age").value;
-  const setAge = document.getElementById("rangeValue");
-  setAge.innerHTML = Age;
-};
+// const showMyAge = () => {
+//   const Age = document.getElementById("age").value;
+//   const setAge = document.getElementById("rangeValue");
+//   setAge.innerHTML = Age;
+// };
 
-const showGenderValue = (gender) => {
-  console.log(document.getElementsByClassName("gender"));
-  document.getElementById("genderValue").innerHTML = gender;
-};
+// const showGenderValue = (gender) => {
+//   console.log(document.getElementsByClassName("gender"));
+//   document.getElementById("genderValue").innerHTML = gender;
+// };
+
+const btn = document.getElementById("button");
+console.log(btn);
+const formd = document.getElementById("formdata");
+console.log(formd);
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data = document.getElementById("formdata");
+  const formData = new FormData(data);
+  const object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  console.log(object);
+});
+
+// const formsData = () => {
+//   const data = document.getElementById("formdata");
+//   const formdata = new FormData(data);
+//   const object = {};
+
+//   console.log(formdata);
+//   formdata.forEach((value, key) => {
+//     object[key] = value; // Map each field name to its value in the object
+//   });
+
+//   console.log(object);
+//   // console.log(object.profilepic.File);
+// };
