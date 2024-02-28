8c30a383263899c00d8da9fb0b0f0e88362a31a7
Suraj Pandey <surazpandey101@gmail.com>
2024-02-29 00:07:22 +0545

    creating login page, register page and working with register password validation

---
+++ Register.js
@@ -0,0 +1,8 @@
+const validation = () => {
+  const password = document.getElementById("exampleInputPassword");
+  const error = document.getElementById("passwordValidation");
+
+  console.log(error);
+
+  //   error.style.display = "none";
+};
