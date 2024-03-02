74f7abcc09e4ad2bc027754c0cdd5be226f2f998
Suraj Pandey <surazpandey101@gmail.com>
2024-03-02 19:39:22 +0545

    working with pagination section

--- home.js
+++ home.js
@@ -5,3 +5,9 @@ const removeClass = () => {
   scroolbar.classList.remove("flex-nowrap");
   showMoreButton.innerText = "";
 };
+
+const changePagination = (index) => {
+  const pages = document.getElementsByClassName("pages");
+  pages[0].removeClass("current");
+  pages[index].classList.add("current");
+};
